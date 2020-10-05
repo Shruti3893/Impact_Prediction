14d77b2ed38220e9a2c405975e470e8de27c4c8a
Shruti3893 <66069960+Shruti3893@users.noreply.github.com>
2020-10-05 15:53:23 +0530

    Files Uploaded for model
    Files Uploaded

---
+++ app.py
@@ -0,0 +1,23 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+
+clf=pickle.load(open('Impact_Prediction.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/impact',methods=['POST'])
+def impact():
+    if request.method == 'POST': 
+     features=[val for val in request.form.values()]
+     prediction = clf.predict(features)
+     
+     return render_template('index.html', prediction_text='Impact will be {}'.format(prediction))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
